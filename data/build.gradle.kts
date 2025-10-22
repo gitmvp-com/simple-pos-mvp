30d290a0ff143344fa6f576c12ce48122c1ac457
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-22 15:47:34 +0300

    Initialize Simple POS MVP with product management feature

---
+++ data/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.library")
+    id("com.google.devtools.ksp")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.gitmvp.data"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ksp {
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = AppConfig.jvmTarget
+    }
+}
+
+dependencies {
+    implementation(project(":common"))
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:${Dependencies.hiltVersion}")
+    kapt("com.google.dagger:hilt-compiler:${Dependencies.hiltVersion}")
+
+    // Database
+    api("androidx.room:room-runtime:${Dependencies.roomVersion}")
+    api("androidx.room:room-ktx:${Dependencies.roomVersion}")
+    ksp("org.xerial:sqlite-jdbc:${Dependencies.sqliteJdbc}")
+    ksp("androidx.room:room-compiler:${Dependencies.roomVersion}")
+
+    // Test
+    testImplementation("junit:junit:${Dependencies.junitVersion}")
+    androidTestImplementation("androidx.test.ext:junit:${Dependencies.junitExtVersion}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${Dependencies.espressoVersion}")
+}
