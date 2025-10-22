a9c878dbf57c8b30be7b2c52b88dd1a2620c2f21
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-22 15:47:21 +0300

    Initialize Simple POS MVP with product management feature

---
+++ buildSrc/src/main/kotlin/AppPlugins.kt
@@ -0,0 +1,7 @@
+object AppPlugins {
+    const val androidApplication = "7.3.0"
+    const val androidLibrary = "7.3.0"
+    const val hilt = "2.44"
+    const val kotlin = "1.7.10"
+    const val ksp = "1.7.10-1.0.6"
+}
